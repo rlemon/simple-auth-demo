ce4b512dabaa1220614e36825d2de1f9556f3e29
Robert Lemon <rob.lemon@gmail.com>
2015-02-17 11:07:36 -0500

    adding some more logs

--- modules/db.js
+++ modules/db.js
@@ -7,9 +7,11 @@ bluebird.promisifyAll(pg.Client.prototype);
 module.exports = function(fn) {
 	var closeConnection;
 	return pg.connectAsync(process.env.DATABASE_URL).spread(function(client, close) {
+		console.log('DB CONNECTED');
 		closeConnection = close;
 		return fn(client);
 	}).finally(function() {
+		console.log('DB TERMINATED');
 		closeConnection();
 	});
 };
